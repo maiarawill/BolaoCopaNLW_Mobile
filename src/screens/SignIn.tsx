2f1ec3695825f6a60dce0ed5cc368d5b9f25bae2
Maiara Cristina Will <maiaracwill@gmail.com>
2022-11-09 19:13:22 -0300

    added useAuth hook to the signIn page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,12 @@
 import { Center, Text, Icon } from "native-base";
 import { Fontisto } from "@expo/vector-icons"
 import Logo from '../assets/logo.svg'
-import { Button } from "../componentes/Button";
+import { Button } from "../components/Buttons";
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn(){
+  const {singIn, user} = useAuth();
+
     return(
     <Center flex={1} bgColor="gray.950" p={7}>
         <Logo width={212} height={40}/>
@@ -12,6 +15,7 @@ export function SignIn(){
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type='SECONDARY'
         mt={12}
+        onPress={singIn}
         />
 
         <Text color="white" textAlign="center" mt={4}>
