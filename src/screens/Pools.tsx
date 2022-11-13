e06b34873b15ed30cef7ddff6897f25d1ccb1152
Maiara Cristina Will <maiaracwill@gmail.com>
2022-11-13 00:13:41 -0300

    created navigation

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,16 +1,23 @@
 import { VStack, Icon } from "native-base";
 
 import { Octicons } from "@expo/vector-icons"
+import {useNavigation} from '@react-navigation/native'
 
 import { Button } from "../components/Buttons";
 import { Header } from "../components/Header";
 
 export function Pools() {
+
+  const {navigate} = useNavigation();
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
       <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
-        <Button leftIcon={<Icon as={Octicons} name="search" color="black" size="md"/>} title="BUSCAR BOLÃO POR CÓDIGO" />
+        <Button 
+        leftIcon={<Icon as={Octicons} name="search" color="black" size="md"/>}
+        title="BUSCAR BOLÃO POR CÓDIGO"
+        onPress={()=> navigate('find')}
+         />
       </VStack>
     </VStack>
   );
